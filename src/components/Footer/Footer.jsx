5b4888ee07fbc4217253f1ff3079426981f83cb7
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-24 00:49:27 -0300

    cria footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyledIcon, StyledLink, HalfWrapper, IconsWrapper, FooterWrapper } from './StyledFooter';
+import { BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs';
+export const Footer = (props) => {
+  return (
+    <FooterWrapper>
+      <HalfWrapper>
+        <IconsWrapper>
+          <StyledLink to={'/cards'}>About</StyledLink>
+          <StyledIcon />
+          <StyledLink to={'/news'}>Contact</StyledLink>
+          <StyledIcon />
+          <StyledLink to={'/login'}>Terms of use</StyledLink>
+          <StyledIcon />
+          <StyledLink to={'/landing'}>Privacy Policy</StyledLink>
+        </IconsWrapper>
+        <p style={{ color: 'grey' }}>Your Website 2022. All Rights Reserved.</p>
+      </HalfWrapper>
+      <HalfWrapper style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+        <BsFacebook style={{ cursor: 'pointer', fontSize: '3rem', color: '#067BFC' }} />
+        <BsTwitter
+          style={{ cursor: 'pointer', fontSize: '3rem', margin: '0rem 2rem', color: '#067BFC' }}
+        />
+        <BsInstagram style={{ cursor: 'pointer', fontSize: '3rem', color: '#067BFC' }} />
+      </HalfWrapper>
+    </FooterWrapper>
+  );
+};
