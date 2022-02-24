d79d1e67bd860e901b0fc902511e6605a8b84b44
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-23 22:44:48 -0300

    atualiza página

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,23 +1,28 @@
-import { Header } from "../../components/Header/Header";
-import { Title } from "../../components/Title/Title";
-import { List } from "../../components/List/List";
-import { SixCards } from "../../components/SixCards/SixCards";
-import { StyledBackgroundWrapper } from "../../components/General-Wrapper/StyledGeneralWrapper";
-import { GlobalStyle } from "../../styles/global";
+import { Header } from '../../components/Header/Header';
+import { Title } from '../../components/Title/Title';
+import { List } from '../../components/List/List';
+import { SixCards } from '../../components/SixCards/SixCards';
+import { StyledBackgroundWrapper } from '../../components/General-Wrapper/StyledGeneralWrapper';
+import { GlobalStyle } from '../../styles/global';
+import { Technologies } from '../../components/Technologies/Technologies';
+import { Comments } from '../../components/Comments/Comments';
 export const Landing = () => {
-    return (
-        <>
-            <GlobalStyle />
-            <StyledBackgroundWrapper>
-                <Header />
-                <Title
-                    title1="Hands-on"
-                    title2="Data Science Course"
-                    subtitle="Led by highly qualified professionals you will learn and practice then put at test your skills. ">
-                    <List />
-                </Title>
-            </StyledBackgroundWrapper>
-            <SixCards />
-        </>
-    );
+  return (
+    <>
+      <GlobalStyle />
+      <StyledBackgroundWrapper>
+        <Header />
+        <Title
+          title1='Hands-on'
+          title2='Data Science Course'
+          subtitle='Led by highly qualified professionals you will learn and practice then put at test your skills. '
+        >
+          <List />
+        </Title>
+      </StyledBackgroundWrapper>
+      <SixCards />
+      <Technologies />
+      <Comments />
+    </>
+  );
 };
