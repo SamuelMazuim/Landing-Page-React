015fcb1c5d7413a2a4f1636d8538e45738e2724e
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-23 22:45:46 -0300

    altera componente 'six-cards'

--- src/components/SixCards/StyledSixCards.jsx
+++ src/components/SixCards/StyledSixCards.jsx
@@ -1,29 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyledCard = styled.div`
-    margin: 1rem;
-    padding: 2rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    text-align: center;
-    /* min-width: 25%; */
-    min-height: 10rem;
-    -webkit-box-shadow: 2px 3px 15px -4px #000000;
-    box-shadow: 2px 3px 15px -4px #000000;
+  margin: 3rem 3rem 1rem 3rem;
+  padding: 2rem;
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  width: 20%;
+  min-height: 10rem;
+  background-color: white;
+  border-radius: 3rem;
+  -webkit-box-shadow: 2px 2px 50px -45px #000000;
+  box-shadow: 2px 2px 50px -45px #000000;
+  transition: 0.4s;
+  :hover {
+    transform: translateY(-0.5rem);
+  }
 `;
 export const StyledCardTitle = styled.h3`
-    width: 100%;
-    margin: 1rem;
-    text-align: left;
-    font-size: 2rem;
-    color: black;
+  width: 100%;
+  margin: 1rem 1rem 0rem;
+  text-align: left;
+  font-size: 2rem;
 `;
 export const StyledP = styled.p`
-    width: 100%;
-    margin: 1rem;
-    text-align: justify;
-    font-size: 1rem;
-    color: black;
+  width: 100%;
+  margin: 1rem;
+  text-align: justify;
+  font-size: 1rem;
 `;
