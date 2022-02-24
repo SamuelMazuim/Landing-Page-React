7f13055e486d6080370337b8c3b386dad72e6686
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-24 00:49:44 -0300

    cria estilo do componente footer

---
+++ src/components/Footer/StyledFooter.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { CgPokemon } from 'react-icons/cg';
+export const StyledLink = styled(Link)`
+  color: #067bfc;
+  text-decoration: none;
+  font-weight: bolder;
+  cursor: pointer;
+`;
+export const IconsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+`;
+export const HalfWrapper = styled.div`
+  width: 50%;
+  padding: 7rem;
+`;
+export const FooterWrapper = styled.div`
+  display: flex;
+  padding: 0rem 6rem;
+`;
+
+export const StyledIcon = styled(CgPokemon)({
+  margin: '0.25rem 0.7rem 0rem',
+  fontSize: '0.6rem',
+  color: 'black',
+});
