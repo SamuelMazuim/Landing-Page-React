614ee448c3492cfe1b5c2537a0096d76709fb184
SamuelMazuim <samuelmazuim@hotmail.com>
2022-02-24 00:51:32 -0300

    Change Layout

--- src/components/Technologies/StyledTechnologies.jsx
+++ src/components/Technologies/StyledTechnologies.jsx
@@ -1,26 +1,27 @@
-import styled from 'styled-components';
-import { StyledCard } from '../SixCards/StyledSixCards';
+import styled from "styled-components";
+import { StyledCard } from "../SixCards/StyledSixCards";
 
 export const StyledCardTitle = styled.h3`
-  margin: 1rem 1rem 0rem;
-  font-size: 2rem;
+    margin: 1rem 1rem 0rem;
+    font-size: 2rem;
 `;
 
 export const StyledSpan = styled.span`
-  color: #8bedff;
+    color: #8bedff;
 `;
 export const StyledTech = styled(StyledCard)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 2rem 1rem 1rem 2rem;
-  padding: 2rem 2rem;
-  width: 15%;
-  border-radius: 1.5rem;
-  border-bottom: solid #8bedff 1.5rem;
-  :hover {
-    border-bottom: solid #096879 1.5rem;
-    transform: translateY(0rem);
-  }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 2rem 1rem 1rem 2rem;
+    padding: 2rem 2rem;
+    width: 15%;
+    border-radius: 1.5rem;
+    border-bottom: solid #276873 1.5rem;
+    :hover {
+        border-bottom: solid #8bedff 1.5rem;
+        transform: translateY(calc(5% - 4px));
+        transition: transform 0.3s cubic-bezier(0.85, 0, 0.15, 1) 0s;
+    }
 `;
