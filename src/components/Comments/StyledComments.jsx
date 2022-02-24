c31f90cd000675b2fb23184f10cc879650c94b11
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-24 00:21:13 -0300

    finaliza estilo do componente 'comments'

--- src/components/Comments/StyledComments.jsx
+++ src/components/Comments/StyledComments.jsx
@@ -1,33 +1,45 @@
 import styled from 'styled-components';
+import { StyledCard } from '../SixCards/StyledSixCards';
 
-export const StyledCard = styled.div`
-  margin: 3rem 3rem 1rem 3rem;
-  padding: 2rem;
+export const StyledImg = styled.div`
+  width: 10rem;
+  height: 10rem;
+  border-radius: 50%;
+  margin-bottom: 1rem;
+  margin-right: -1rem;
+  background-image: ${(props) => props.bg};
+  background-size: cover;
+  background-position: ${(props) => props.pos}; ;
+`;
+
+export const StyledVerticalWrapper = styled.div`
   display: flex;
-  align-items: baseline;
-  justify-content: center;
   flex-direction: column;
-  text-align: center;
-  width: 20%;
-  min-height: 10rem;
-  background-color: white;
-  border-radius: 3rem;
-  -webkit-box-shadow: 2px 2px 50px -45px #000000;
-  box-shadow: 2px 2px 50px -45px #000000;
-  transition: 0.4s;
+  padding: 0rem 2rem;
+`;
+export const StyledHorizontalWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const StyledComment = styled(StyledCard)`
+  transition: 0s;
   :hover {
-    transform: translateY(-0.5rem);
+    transform: translateY(0rem);
   }
 `;
-export const StyledCardTitle = styled.h3`
-  width: 100%;
-  margin: 1rem 1rem 0rem;
-  text-align: left;
-  font-size: 2rem;
+export const StyledLine = styled.span`
+  border-bottom: solid 0.3rem #8bedff;
+  margin: 0.3rem 0rem 0.4rem 1rem;
+  width: 3rem;
 `;
-export const StyledP = styled.p`
-  width: 100%;
-  margin: 1rem;
+export const StyledCommentP = styled.p`
+  width: 90%;
+  margin: 1rem 1rem 1rem 1rem;
   text-align: justify;
   font-size: 1rem;
+`;
+export const StyledProf = styled.p`
+  margin: 0rem 1rem 1rem 1rem;
+  text-align: left;
+  color: grey;
 `;
