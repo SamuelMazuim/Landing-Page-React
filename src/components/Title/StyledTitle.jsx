3f1f007e4e0ad9e10fa4fec422b1f5dd9707bcdc
Crhistian <crhistiandeoliveira@gmail.com>
2022-02-24 12:15:32 -0300

    adiciona animação no ícone principaç

--- src/components/Title/StyledTitle.jsx
+++ src/components/Title/StyledTitle.jsx
@@ -1,59 +1,70 @@
-import styled from "styled-components";
-import { GiMaterialsScience } from "react-icons/gi";
+import styled from 'styled-components';
+import { GiMaterialsScience } from 'react-icons/gi';
 
 export const StyledCentralizeWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
 `;
 export const StyledCollumnWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-left: 3rem;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 3rem;
+  width: 100%;
 `;
 
 export const StyledH1 = styled.h1`
-    width: 35rem;
-    text-align: left;
-    font-family: sans-serif;
-    font-size: 3.5rem;
+  width: 35rem;
+  text-align: left;
+  font-family: sans-serif;
+  font-size: 3.5rem;
 `;
 
 export const StyledH2 = styled.h2`
-    width: 30rem;
-    text-align: left;
-    margin-top: 1rem;
-    font-family: sans-serif;
-    font-size: 1.7rem;
+  width: 30rem;
+  text-align: left;
+  margin-top: 1rem;
+  font-family: sans-serif;
+  font-size: 1.7rem;
 `;
 
 export const StyledIcon = styled(GiMaterialsScience)`
-    font-size: 30rem;
-    color: #8bedff;
-    /* transform: scaleX(-1);
-    transition: 0.5s; */
+  font-size: 30rem;
+  color: #8bedff;
+  animation: rotate 4s linear infinite;
+  @keyframes rotate {
+    0 {
+      transform: rotateY(0);
+    }
+    50% {
+      transform: rotateY(180deg);
+    }
+    100% {
+      transform: rotateY(0deg);
+    }
+  }
 `;
 
 export const StyledButton = styled.button`
-    margin-bottom: 2rem;
-    box-shadow: 0px 10px 14px -7px #276873;
-    background: linear-gradient(to bottom, #0dccef 5%, #255b64 100%);
-    background-color: #0dccef;
-    border-radius: 8px;
-    display: inline-block;
-    cursor: pointer;
-    color: #ffffff;
-    font-family: Arial;
-    font-size: 20px;
-    font-weight: bold;
-    padding: 13px 32px;
-    text-decoration: none;
-    text-shadow: 0px 1px 0px #3d768a;
-    :hover {
-        background: linear-gradient(to bottom, #8bedff 5%, #2db0c7 100%);
-        transform: scale(1.1) perspective(1px);
-    }
+  margin-bottom: 2rem;
+  width: 60%;
+  box-shadow: 0px 10px 14px -7px #276873;
+  background: linear-gradient(to bottom, #0dccef 5%, #255b64 100%);
+  background-color: #0dccef;
+  border-radius: 8px;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: sans-serif;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 13px 32px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #3d768a;
+  :hover {
+    background: linear-gradient(to bottom, #8bedff 5%, #2db0c7 100%);
+    transform: scale(1.1) perspective(1px);
+  }
 `;
--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,40 +1,39 @@
-import React from "react";
-import { GeneralWrapper } from "../General-Wrapper/GeneralWrapper";
+import React from 'react';
+import { GeneralWrapper } from '../General-Wrapper/GeneralWrapper';
 import {
-    StyledCentralizeWrapper,
-    StyledCollumnWrapper,
-    StyledH1,
-    StyledH2,
-    StyledIcon,
-    StyledButton,
-    StyledLocalWrapper,
-} from "./StyledTitle";
+  StyledCentralizeWrapper,
+  StyledCollumnWrapper,
+  StyledH1,
+  StyledH2,
+  StyledIcon,
+} from './StyledTitle';
 
 export const Title = (props) => {
-    return (
-        <>
-            <StyledCentralizeWrapper
-                style={{
-                    color: "white",
-                }}>
-                <GeneralWrapper>
-                    <StyledCollumnWrapper
-                        style={{
-                            marginLeft: "8rem",
-                        }}>
-                        <StyledH1>{props.title1}</StyledH1>
-                        <StyledH1>{props.title2}</StyledH1>
-                        <StyledH2>{props.subtitle}</StyledH2>
-                        {props.children}
-                    </StyledCollumnWrapper>
-                </GeneralWrapper>
-                <StyledCentralizeWrapper>
-                    <StyledIcon />
-                </StyledCentralizeWrapper>
-            </StyledCentralizeWrapper>
-            <StyledCentralizeWrapper>
-                <StyledButton>Start Now</StyledButton>
-            </StyledCentralizeWrapper>
-        </>
-    );
+  return (
+    <>
+      <div style={{ padding: '3rem 0rem' }}>
+        <StyledCentralizeWrapper
+          style={{
+            color: 'white',
+          }}
+        >
+          <GeneralWrapper>
+            <StyledCollumnWrapper
+              style={{
+                marginLeft: '8rem',
+              }}
+            >
+              <StyledH1>{props.title1}</StyledH1>
+              <StyledH1>{props.title2}</StyledH1>
+              <StyledH2>{props.subtitle}</StyledH2>
+              {props.children}
+            </StyledCollumnWrapper>
+          </GeneralWrapper>
+          <StyledCentralizeWrapper>
+            <StyledIcon />
+          </StyledCentralizeWrapper>
+        </StyledCentralizeWrapper>
+      </div>
+    </>
+  );
 };
